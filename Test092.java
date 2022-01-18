3a47c3511c049c200ec3510f355c35521519e9ce
eunhye0606 <cherry5813@naver.com>
2022-01-18 17:53:11 +0900

    20220118 오후 배열

--- Test092.java
+++ Test092.java
@@ -37,20 +37,60 @@ public class Test092
 {
 	public static void main(String[] args)
 	{
+		//배열의 배열 선언 및 메모리 할당
+		char[][] arr = new char[5][5];
+
+		char start = 'A';
+
+		for (int i=0;i<5 ;i++ )  // i → 0 1 2 3 4
+		{
+			for (int j=4;j>=0 ;j-- )   // j → 4 3 2 1 0
+			{
+				arr[j][i] = start++;
+
+			}
+		}
+		//배열 출력
+		for (int i=0;i<arr.length ;i++ )
+		{
+			for (int j=0;j<arr[i].length ;j++ )
+			{
+				System.out.printf("%4c",arr[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
+
+/*
+   E   J   O   T   Y
+   D   I   N   S   X
+   C   H   M   R   W
+   B   G   L   Q   V
+   A   F   K   P   U
+계속하려면 아무 키나 누르십시오 . . .
+*/
+
+/*
+public class Test092
+{
+	public static void main(String[] args)
+	{
 		//배열 구성.
 		char [][] arr = new char [5][5];
 
 		int n=65;
-		// 방에 들어가는 문자들 아스키코드 65 ~90.
+		// 방에 들어가는 문자들 아스키코드 65 ~
 		for (int i = 0;i<arr.length ;i++ )
 		{
-			for (int j=4; j>=0 ;j--) //43210
+			for (int j=4; j>=0 ;j--,n++) //43210
 			{
 				arr[j][i] = (char)n;   
-				n++;
+				//n++;
 				
 
 			}
+			//System.out.println(n);
 		}
 
 		//배열 출력
@@ -65,6 +105,7 @@ public class Test092
 		
 	}
 }
+*/
 
 /*
    E   J   O   T   Y
