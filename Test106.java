219e78e2c063d22a086d3b81e4562c6095844171
eunhye <cherry5813@naver.com>
2022-02-02 23:06:26 +0900

    복습

--- Test106.java
+++ Test106.java
@@ -12,22 +12,22 @@
 					  //-- 반환이 없든 (void)이든 double이든 자바는 몰라
 		 double result = print(3.14);
 	 }
-	 public static void print() {}
+	// public static void print() {}
 	 //public static void print() {} //--default (x)
 
-	 public static void print(int i) {}
+	// public static void print(int i) {}
 	 //public static void print(int j) {} //-- 이건 안돼 (x)
 	 								    //-- 밖에서 이름붙히는게 아님
 
-	 public static void print(char c) {}    //-- 일단 됨. 근데 자동형변환 규칙 check~!!
+	// public static void print(char c) {}    //-- 일단 됨. 근데 자동형변환 규칙 check~!!
 	 
-	 public static void print(int i,int j) {}
+	// public static void print(int i,int j) {}
 
-	 public static void print(double d) {}
+	// public static void print(double d) {}
 
 	 //public static void print(double e) {return 10.0} //--정의 불가 void인데 return자료형 반환이라
 	
 
-	 //public static double print(double e) {return 10.0;}//--(x) check~!!
+	 public static double print(double e) {return 10.0;}//--(x) check~!!
 
  } 
